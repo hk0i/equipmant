b58ac8037900a90b6c93f0227c16726c39b8c5e1
Gregory McQuillan <gmcquillan@gmail.com>
2008-12-01 01:43:51 +0000

    Added tabs and changed recent menu to show parent directory. v3.1.0 (see changelog for details)

--- src/imp_equipmant.cpp
+++ src/imp_equipmant.cpp
@@ -23,7 +23,8 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 void Imp_equipmant::fileNew(void)
 {
 	//std::cerr << this->height() << "x" << this->width() << std::endl;
-	fileClose();
+	int newTabID = tabFiles->addTab(new equipTab(),"Untitled");
+	tabFiles->setCurrentIndex(newTabID);
 	statusbar->showMessage("Editing new document");
 }
 
@@ -31,24 +32,49 @@ void Imp_equipmant::fileOpen(void)
 {
 	QString fileName = QFileDialog::getOpenFileName(this, "Open Equip Macro...",
 														myLastFileDir, "Equipment Macros (*.txt *.equip)");
+	if (fileName.isEmpty())
+		return;
+
+	//check if file is already open :)
+	equipTab *tab=0;
+ 	for (int i=0; i < tabFiles->count(); i++)
+	{
+		tab = (equipTab*)tabFiles->widget(i);
+		if (tab->getCurrentFile() == fileName)
+		{
+			tabFiles->setCurrentIndex(i);
+			return;
+		}
+	}
+
+
+	int newTabNum = tabFiles->addTab(new equipTab(),nameFromPath(fileName));
+	tabFiles->setCurrentIndex(newTabNum);
 	readFile(fileName);
-	txtTextMode->setText(generateText());
+	CTAB
+	cTab->txtTextMode->setText(cTab->generateText());
 }
 
 void Imp_equipmant::fileClose(void)
 {
-	if (myCurrentFile.isEmpty())
+	CTAB
+	if (!cTab)
+		return;
+	if (cTab->getCurrentFile().isEmpty())
 		statusbar->showMessage("Document closed.");
 	else
-		statusbar->showMessage("Closed File: " + myCurrentFile);
-	setCurrentFile("");
-	clearFields();
+		statusbar->showMessage("Closed File: " + cTab->getCurrentFile());
+	cTab->setCurrentFile("");
+
+	// **** WARNING - I THINK THE ACTUAL WIDGETS NEED TO BE REMOVED ALSO ****
+	tabFiles->removeTab(tabFiles->currentIndex());
+	// **** WARNING - I THINK THE ACTUAL WIDGETS NEED TO BE REMOVED ALSO ****
 }
 
 void Imp_equipmant::fileSave(void)
 {
-	myExtraData = txtExtraData->toPlainText();
-	if (myCurrentFile.isEmpty())
+	CTAB
+	if (cTab->getCurrentFile().isEmpty())
 	{
 		fileSaveAs();
 	}
@@ -62,8 +88,7 @@ void Imp_equipmant::fileSaveAs(void)
 	//pop dialog
 	QString fileName = QFileDialog::getSaveFileName(this, "Save Equip Macro As...",
 													myLastFileDir, "Equipment Macros (*.txt *.equip)");
-	myExtraData = txtExtraData->toPlainText();
-													
+	CTAB
 	//check for a valid file...
 	if (!fileName.isEmpty())
 	{
@@ -82,7 +107,7 @@ void Imp_equipmant::fileSaveAs(void)
 	else
 		statusbar->showMessage("No filename selected. File has NOT been saved.");
 	
-	setCurrentFile(fileName);
+	cTab->setCurrentFile(fileName);
 }
 
 void Imp_equipmant::fileQuit(void)
@@ -119,122 +144,25 @@ void Imp_equipmant::helpAbout(void)
 }
 
 //view slots
-void Imp_equipmant::viewEdit(void) { swView->setCurrentIndex(0); }
-void Imp_equipmant::viewOutput(void) { swView->setCurrentIndex(1); }
-
-void Imp_equipmant::viewModeChanged(int newPage)
-{
-	myExtraData = txtExtraData->toPlainText();
-	txtTextMode->setText(generateText());
-	if (newPage == 0)
-	{
-		myEditMode = "(Edit Mode)";
-		updateTitle();
-	}
-	else
-	{
-		myEditMode = "(View Output)";
-		updateTitle();
-	}
-
+void Imp_equipmant::viewEdit(void) { 
+	CTAB
+	if (cTab)
+		cTab->swView->setCurrentIndex(0);
+}
+void Imp_equipmant::viewOutput(void) { 
+	CTAB
+	if (cTab)
+		cTab->swView->setCurrentIndex(1);
 }
 
-//button slots
-void Imp_equipmant::writeFile_clicked(void)
-{
-	fileSave();
-}
-
-// extra data widgets
-void Imp_equipmant::addToBin_clicked(void)
-{
-		/*txtExtraData->setText(txtExtraData->toPlainText() + "\r\n");
-	txtExtraData->setText(txt	*/
-	/*if (!txtExtraData->toPlainText().isEmpty())
-		addToDataBin("\r\n");*/
-	QString str;
-	QTextStream qts(&str);
-	bool ok = false;	//dummy variable used for dialog and commands
-	int mult = 1;		//timeout multiplier
-
-	if (cmbExtraType->currentIndex() == 0)
-	{
-		addToDataBin("input ");
-
-		switch (cmbExtraCommand->currentIndex())
-		{
-			case 0:
-			default:
-				str = "/ja ";
-				break;
-
-			case 1:
-				str = "/ma ";
-				break;
-
-			case 2:
-				str = "/echo ";
-				break;
-
-			case 3:
-				str = "/party ";
-				break;
-
-			case 4:
-				str = "/linkshell ";
-				break;
-		}
-			
-		if (cmbExtraCommand->currentIndex() <= 1)
-			ok = true;
-		if (ok)	str += '"';
-		str += cmbExtraParam->currentText();
-		if (ok)	str += "\" " + cmbExtraTarget->currentText();
-		str	+= "\r\n";
-		addToDataBin(str);
-	}
-	else
-	{
-		if (spnTimeout->value() > 0) {
-			switch (cmbTimetype->currentIndex())
-			{
 
 
-				case 1:
-					mult = 60;
-					break;
-
-				case 2:
-					mult = 60*60;
-					break;
-
-				case 0:
-				default:
-					mult = 1;
-					break;
-			}
-
-			qts << "pause " << (spnTimeout->value() * mult) << "\r\n";
-			addToDataBin(str);
-		}
-		else {
-			int ret = QMessageBox::warning(this,tr("No timeout specified!"),
-					tr("No pause time specified.\nNo changes have been made."),
-					QMessageBox::Ok);
-
-		}
-	}
-}
-
-void Imp_equipmant::extraCmd_changed(int newIndex)
-{
-	if (newIndex > 1)
-		cmbExtraTarget->setHidden(true);
-	else
-		cmbExtraTarget->setVisible(true);
-}
 
 //tab slots
+/*void Imp_equipmant::tabFiles_changed(int)
+{
+	updateTitle();
+}*/
 /*void Imp_equipmant::tabText_changed(void)
 {
 	txtTextMode->setText(generateText());
@@ -244,105 +172,20 @@ void Imp_equipmant::extraCmd_changed(int newIndex)
 //private functions
 void Imp_equipmant::writeFile(void)
 {
-	if(!myCurrentFile.isEmpty())
+	CTAB
+	if(!cTab->getCurrentFile().isEmpty())
 	{
-		writeFile(myCurrentFile);
+		writeFile(cTab->getCurrentFile());
 	}
 	
 }
 
-QString Imp_equipmant::generateText(void)
-{
-	QString result;
-	QTextStream strOut(&result);
-	
-	//windows line terminations \r\n (since FF is a PC/windows game)
-	
-	// if a text field is empty, it will write a commented line,
-	// this line acts as a placeholder, to make the layout of the
-	// files uniform. It is commented so that equipment won't be
-	// removed from the macro.
-
-	strOut << EQM_OPC1;	//Equipmant output comment1
-	strOut << EQM_OPC2; //equipmant output comment2 - both are #defined in imp_equipmant.h
-	if (txtMain->text().isEmpty())
-		COMMENT
-	strOut << "input /equip main \"" << txtMain->text() << "\"\r\n";
-
-	if (txtSub->text().isEmpty())
-		COMMENT
-	strOut << "input /equip sub \"" << txtSub->text() << "\"\r\n";
-
-	if (txtRanged->text().isEmpty())
-		COMMENT
-	strOut << "input /equip range \"" << txtRanged->text() << "\"\r\n";
-
-	if (txtAmmo->text().isEmpty())
-		COMMENT
-	strOut << "input /equip ammo \"" << txtAmmo->text() << "\"\r\n";
-
-	if (txtHead->text().isEmpty())
-		COMMENT
-	strOut << "input /equip head \"" << txtHead->text() << "\"\r\n";
-	
-	if (txtNeck->text().isEmpty())
-		COMMENT
-	strOut << "input /equip neck \"" << txtNeck->text() << "\"\r\n";
-	
-	if (txtLEar->text().isEmpty())
-		COMMENT
-	strOut << "input /equip L.Ear \"" << txtLEar->text() << "\"\r\n";
-	 
-	if (txtREar->text().isEmpty())
-		COMMENT
-	strOut << "input /equip R.Ear \"" << txtREar->text() << "\"\r\n";
-
-	if (txtBody->text().isEmpty())
-		COMMENT
-	strOut << "input /equip body \"" << txtBody->text() << "\"\r\n";
-	
-	if (txtHands->text().isEmpty())
-		COMMENT
-	strOut << "input /equip hands \"" << txtHands->text() << "\"\r\n";
-	
-	if (txtLRing->text().isEmpty())
-		COMMENT
-	strOut << "input /equip L.Ring \"" << txtLRing->text() << "\"\r\n";
-	
-	if (txtRRing->text().isEmpty())
-		COMMENT
-	strOut << "input /equip R.Ring \"" << txtRRing->text() << "\"\r\n";
-	
-	if (txtBack->text().isEmpty())
-		COMMENT
-	strOut << "input /equip back \"" << txtBack->text() << "\"\r\n";
-	
-	if (txtWaist->text().isEmpty())
-		COMMENT
-	strOut << "input /equip waist \"" << txtWaist->text() << "\"\r\n";
-	
-	if (txtLegs->text().isEmpty())
-		COMMENT
-	strOut << "input /equip legs \"" << txtLegs->text() << "\"\r\n";
-	
-	if (txtFeet->text().isEmpty())
-		COMMENT
-	strOut << "input /equip feet \"" << txtFeet->text() << "\"\r\n";
-	
-	//EXTRA DATA STUFF - added in 1.2.3
-	if (!myExtraData.isEmpty())
-	{
-		strOut << "\r\n" << EQM_EXTRADATA << myExtraData;
-	}
-	
-	return result;
-}
 
 QString Imp_equipmant::nameFromPath(QString fullPath)
 {
 	QString ret(QDir::fromNativeSeparators(fullPath));
 	int lastSlash = -1;
-	lastSlash = ret.lastIndexOf('/');
+	lastSlash = ret.lastIndexOf('/', ret.lastIndexOf('/')-1);
 	return ret.mid(lastSlash+1);
 }
 
@@ -351,8 +194,10 @@ void Imp_equipmant::writeFile(QString fName)
 	QFile file(fName);
 	file.open(QIODevice::WriteOnly);
 	QTextStream fout(&file);
+
+	CTAB
 	
-	fout << generateText();
+	fout << cTab->generateText();
 	
 	//set last file path
 	myLastFileDir = fName.left(fName.lastIndexOf('/'));
@@ -364,219 +209,38 @@ void Imp_equipmant::writeFile(QString fName)
 
 void Imp_equipmant::readFile(QString fileName)
 {
-	QFile inFile(fileName);
-	if (!inFile.exists())
+	QFile check(fileName);
+	if (!check.exists())
 		return;
-	inFile.open(QFile::ReadOnly);
-	
-	setCurrentFile(fileName);
-	//set last file path
-	myLastFileDir = fileName.left(fileName.lastIndexOf('/'));
-	saveData();		//save data now, in case of a program crash ;D
-	
-	
-	//clear fields to prove test cases.
-	clearFields();
-	
-	//clear textmode area.
-	txtTextMode->clear();
-	
-	char buff[1024];
-	QString tmp;
-	QString parseCheck;	//used to check for Left/Right equips. i.e., if file contains
-						// L.Ring or ring1 etc..
-	qint64 len = 1;
-	int firstQuote = 0;
-	
-	//EXTRA DATA CODE
-	myExtraData.clear();	//clear extra data variable
-	
-	
-	//version checking (commented out for removal)
-	
-	//len = inFile.readLine(buff, sizeof(buff));
-	//tmp = buff;
-	
-	//if (!tmp.contains("_EV10_"))
-	//	return;
-	
-	while (len > 0)
-	{
-		len = inFile.readLine(buff, sizeof(buff));
-		tmp = buff;
-		firstQuote = 0;
-		
-		if (len > 0)
-		{
-		
-				//clear parsecheck.
-				parseCheck = "";
-				
-				//EXTRA DATA CHECKING
-				/* this part here is added to make sure all extra data that isn't part
-				   of a standard Equipmant generated file isn't just thrown out. */
-				//Extra comments checking code:
-				if (tmp.trimmed().left(2) == "//") {
-					//if it's not our standrad first two lines of comments,
-					if (tmp.left(11) != "//Equipmant" &&
-						tmp.left(5) != "//Get" &&
-						tmp.trimmed() != "//Extra data found in file:" &&
-						!tmp.contains("input /equip")) {
-						myExtraData += tmp.trimmed() + "\r\n";
-						std::cerr << "[Extra Data Found]: \"" <<
-									 tmp.trimmed().toStdString().c_str() << "\"" << std::endl;
-						
-					}
-				}
-				else {
-					//if it DOESN'T begin with a // and DOESN'T contain input /equip
-					if (!tmp.contains("input /equip")) {
-						//fixing a random blank line bug.
-						if (!tmp.trimmed().isEmpty()) {
-							myExtraData += tmp.trimmed() + "\r\n";				//add it to extra data ;)
-							std::cerr << "[Extra Data Found2]: \"" <<
-										 tmp.trimmed().toStdString().c_str() << "\"" << std::endl;
-						}
-					}
-					
-				
-				}
-				
-				
-				if (txtMain->text().isEmpty())
-					txtMain->setText(parsePiece(tmp,"main"));
-				if (txtSub->text().isEmpty())
-					txtSub->setText(parsePiece(tmp,"sub"));
-				if (txtRanged->text().isEmpty())
-					txtRanged->setText(parsePiece(tmp, "range"));
-				if (txtAmmo->text().isEmpty())
-					txtAmmo->setText(parsePiece(tmp, "ammo"));
-				if (txtHead->text().isEmpty())
-					txtHead->setText(parsePiece(tmp, "head"));
-				if (txtNeck->text().isEmpty())
-					txtNeck->setText(parsePiece(tmp, "neck"));
+	CTAB
 
-				//left and right pieces require some scratch space
-				//to check if L.Ear or ear1 is present.
-				if (txtLEar->text().isEmpty())
-				{
-					parseCheck = parsePiece(tmp,"l.ear");
-					if (parseCheck.isEmpty())
-						txtLEar->setText(parsePiece(tmp,"ear1"));
-					else
-						txtLEar->setText(parseCheck);
-				}
-				if (txtREar->text().isEmpty())
-				{
-					parseCheck = parsePiece(tmp,"r.ear");
-					if (parseCheck.isEmpty())
-						txtREar->setText(parsePiece(tmp,"ear2"));
-					else
-						txtREar->setText(parseCheck);
-				}
-				//
-
-				if (txtBody->text().isEmpty())
-					txtBody->setText(parsePiece(tmp, "body"));
-				if (txtHands->text().isEmpty())
-					txtHands->setText(parsePiece(tmp, "hands"));
-				
-				//again with lefts, 1s, rights and 2s
-				if (txtLRing->text().isEmpty())
-				{
-					parseCheck = parsePiece(tmp,"l.ring");
-					if (parseCheck.isEmpty())
-						txtLRing->setText(parsePiece(tmp,"ring1"));
-					else
-						txtLRing->setText(parseCheck);
-				}
-				if (txtRRing->text().isEmpty())
-				{
-					parseCheck = parsePiece(tmp,"r.ring");
-					if (parseCheck.isEmpty())
-						txtRRing->setText(parsePiece(tmp,"ring2"));
-					else
-						txtRRing->setText(parseCheck);
-				}
-				//
-
-
-				if (txtBack->text().isEmpty())
-					txtBack->setText(parsePiece(tmp, "back"));
-				if (txtWaist->text().isEmpty())
-					txtWaist->setText(parsePiece(tmp, "waist"));
-				if (txtLegs->text().isEmpty())
-					txtLegs->setText(parsePiece(tmp, "legs"));
-				if (txtFeet->text().isEmpty())
-					txtFeet->setText(parsePiece(tmp, "feet"));
-				
-		}
-		else	//len > 0
-			break;
-		
-	}	//while loop
-
-	txtExtraData->setText(myExtraData);
+	cTab->readFile(fileName);
 	
 	statusbar->showMessage("Loaded File: " + fileName);
 	addRecent(fileName);
-}
-
-void Imp_equipmant::clearFields(void)
-{
-	txtMain->clear();
-	txtSub->clear();
-	txtRanged->clear();
-	txtAmmo->clear();
-	
-	txtHead->clear();
-	txtNeck->clear();
-	txtLEar->clear();
-	txtREar->clear();
-	
-	txtBody->clear();
-	txtHands->clear();
-	txtLRing->clear();
-	txtRRing->clear();
-	
-	txtBack->clear();
-	txtWaist->clear();
-	txtLegs->clear();
-	txtFeet->clear();
-
-	txtExtraData->clear();
-	txtTextMode->clear();
-
-	viewModeChanged(swView->currentIndex());
-	
 }
 
 void Imp_equipmant::updateTitle(void)
 {
 	QString title;
 	QTextStream ts(&title);
-	if (myCurrentFile.isEmpty())
+	CTAB
+	if (cTab->getCurrentFile().isEmpty())
 		ts << "Equipmant Equipment Macro Manager v" << EQM_VERSION << " [New File] " << myEditMode;
 	else
-		ts << "Equipmant Equipment Macro Manager [" << nameFromPath(myCurrentFile) << "] " << myEditMode;
+		ts << "Equipmant Equipment Macro Manager [" << nameFromPath(cTab->getCurrentFile()) << "] " << myEditMode;
 	this->setWindowTitle(title);
 }
 
-void Imp_equipmant::updateTitle(QString newTitle)
+/*void Imp_equipmant::setCurrentFile(QString newFile)
 {
-	//this method is now deprecated :D
-	updateTitle();
-	//this->setWindowTitle(this->windowTitle() + ' ' + newTitle + ' ' + myEditMode);
-}
-
-void Imp_equipmant::setCurrentFile(QString newFile)
-{
-	myCurrentFile = newFile;
+	CTAB
+	cTab->setCurrentFile(newFile);
 	if (newFile.isEmpty())		// if the new file is empty, set the title to say "No File"
 		updateTitle("[New File]");
 	else
-		updateTitle("[" + nameFromPath(myCurrentFile) + "]");
-}
+		updateTitle("[" + nameFromPath(cTab->getCurrentFile()) + "]");
+}*/
 
 void Imp_equipmant::saveData(void)
 {
@@ -748,28 +412,4 @@ void Imp_equipmant::openRecent(int recNum)
 	readFile(myRecentDocs[recNum-1]);
 }
 
-void Imp_equipmant::addToDataBin(QString textToAdd)
-{
-	txtExtraData->setText(txtExtraData->toPlainText() + textToAdd);
-}
 
-QString Imp_equipmant::parsePiece(QString pString, QString loc)
-{
-	//this function has been tested and it works.
-	QString result = "";
-	int firstQuote = 0;
-	//trim excess space and eat carriage returns.
-	pString = pString.trimmed();
-	//trim quotes
-	pString = pString.left(pString.length() - 1);
-	if (pString.contains("input /equip " + loc, Qt::CaseInsensitive))
-	{
-		//get the second to last quote, aka the FIRST quote of our "equipment_piece"
-		firstQuote = pString.lastIndexOf('"');
-		//subtract 4 below; 2 for \r\n and 2 for both quotes.
-		result = pString.mid(firstQuote + 1, -1);
-	}
-	
-	return result;
-	
-}
